6a8981864c9ad6a9374d29495efd12023a53f6ca
nikossyr <b_eliever@hotmail.com>
2020-08-21 15:57:31 +0300

    Implemented add new transaction functionality. The new transaction is added in the list, in the GlobalContext and the amounts of Balance, IncomeExpense are calculated automatically.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,8 +1,23 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
+import {GlobalContext} from "../context/GlobalState";
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const {addTransaction} = useContext(GlobalContext)
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount
+        };
+
+        addTransaction(newTransaction);
+        setText('');
+        setAmount(0);
+    }
     return (
         <>
             <h3>Add new transaction</h3>
@@ -20,7 +35,7 @@ export const AddTransaction = () => {
                     <input type="number" value={amount} onChange={(e) => setAmount(e.target.valueAsNumber)}
                            placeholder="Enter amount..."/>
                 </div>
-                <button className="btn">Add transaction</button>
+                <button className="btn" onClick={onSubmit}>Add transaction</button>
             </form>
         </>
     )
