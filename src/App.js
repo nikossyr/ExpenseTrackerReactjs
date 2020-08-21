29565fc9a82c2a528ac6e9c9d73c88790d59ed74
nikossyr <b_eliever@hotmail.com>
2020-08-21 15:23:14 +0300

    Implemented GlobalContext and populated transaction list items with transaction values from GlobalContext. Also implemented ternary rendering of items considering whether it is an expense or income.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {Balance} from "./components/Balance";
 import {IncomeExpenses} from "./components/IncomeExpenses";
 import {TransactionList} from "./components/TransactionList";
 import {AddTransaction} from "./components/AddTransaction";
+import {GlobalProvider} from "./context/GlobalState";
 import './App.css';
 
 
 function App() {
     return (
-        <div>
+        <GlobalProvider>
             <Header/>
             <div className="container">
                 <Balance/>
@@ -17,8 +18,8 @@ function App() {
                 <TransactionList/>
                 <AddTransaction/>
             </div>
-        </div>
-  );
+        </GlobalProvider>
+    );
 }
 
 export default App;
